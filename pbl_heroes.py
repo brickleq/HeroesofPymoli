b9e416bcab92ce0e06c96bf8b1469d9e9fc13449
brickleq <pblequire@gmail.com>
2019-07-20 15:04:07 -0500

    solved

--- pbl_heroes.py
+++ pbl_heroes.py
@@ -1,75 +1,5 @@
-#%%
-'''
-
-### Player Count
-
-* Total Number of Players
-
-### Purchasing Analysis (Total)
-
-* Number of Unique Items
-* Average Purchase Price
-* Total Number of Purchases
-* Total Revenue
-
-### Gender Demographics
-
-* Percentage and Count of Male Players
-* Percentage and Count of Female Players
-* Percentage and Count of Other / Non-Disclosed
-
-### Purchasing Analysis (Gender)
-
-* The below each broken by gender
-  * Purchase Count
-  * Average Purchase Price
-  * Total Purchase Value
-  * Average Purchase Total per Person by Gender
-
-### Age Demographics
-
-* The below each broken into bins of 4 years (i.e. &lt;10, 10-14, 15-19, etc.)
-  * Purchase Count
-  * Average Purchase Price
-  * Total Purchase Value
-  * Average Purchase Total per Person by Age Group
-
-### Top Spenders
-
-* Identify the the top 5 spenders in the game by total purchase value, then list (in a table):
-  * SN
-  * Purchase Count
-  * Average Purchase Price
-  * Total Purchase Value
-
-### Most Popular Items
-
-* Identify the 5 most popular items by purchase count, then list (in a table):
-  * Item ID
-  * Item Name
-  * Purchase Count
-  * Item Price
-  * Total Purchase Value
-
-### Most Profitable Items
-
-* Identify the 5 most profitable items by total purchase value, then list (in a table):
-  * Item ID
-  * Item Name
-  * Purchase Count
-  * Item Price
-  * Total Purchase Value
-
-As final considerations:
-
-* You must use the Pandas Library and the Jupyter Notebook.
-* You must submit a link to your Jupyter Notebook with the viewable Data Frames.
-* You must include a written description of three observable trends based on the data.
-* See [Example Solution](HeroesOfPymoli/HeroesOfPymoli_starter.ipynb) for a reference on expected format.
-'''
 ##%
 # Dependencies
-import numpy as np
 import pandas as pd
 #%%
 purchaseData = pd.DataFrame
@@ -262,6 +192,19 @@ print('     > 44: $' + str(round(avgPrice,2)) + '0')
 #   * Average Purchase Price
 #   * Total Purchase Value
 
+purchaseCount = purchaseData.groupby(['SN']).count()['Price'].rename('Purchase Count')
+purchaseCount.head()
+purchaseValue = purchaseData.groupby(['SN']).sum()['Price'].rename('Total Purchase Value')
+purchaseValue.head()
+averagePurchase = purchaseData.groupby(['SN']).mean()['Price'].rename('Average Purchase Price')
+averagePurchase.head()
+
+topSpenders = pd.DataFrame({'Purchase Count':purchaseCount,
+'Total Purchase Value': purchaseValue,
+'Average Purchase Price': averagePurchase})
+topSpenders.sort_values(['Total Purchase Value'], ascending=False).head()
+
+#%%
 ### Most Popular Items
 
 # * Identify the 5 most popular items by purchase count, then list (in a table):
@@ -271,6 +214,16 @@ print('     > 44: $' + str(round(avgPrice,2)) + '0')
 #   * Item Price
 #   * Total Purchase Value
 
+popularItemsCount = purchaseData.groupby(['Item Name']).count()['Item ID'].rename('Purchase Count')
+popularItemsPrice = purchaseData.groupby(['Item Name']).count()['Price'].rename('Item Price')
+popularItemsValue = purchaseData.groupby(['Item Name']).sum()['Price'].rename('Total Purchase Value')
+mostPopularItems = pd.DataFrame({'Purchase Count': popularItemsCount,
+'Item Price': popularItemsPrice,
+'Total Purchase Value': popularItemsValue})
+mostPopularItems.sort_values(['Purchase Count'], ascending=False).head()
+
+
+#%%
 ### Most Profitable Items
 
 # * Identify the 5 most profitable items by total purchase value, then list (in a table):
@@ -279,3 +232,7 @@ print('     > 44: $' + str(round(avgPrice,2)) + '0')
 #   * Purchase Count
 #   * Item Price
 #   * Total Purchase Value
+mostProfitableItems = pd.DataFrame({'Purchase Count': popularItemsCount,
+'Item Price': popularItemsPrice,
+'Total Purchase Value': popularItemsValue})
+mostProfitableItems.sort_values(['Total Purchase Value'], ascending=False).head()
